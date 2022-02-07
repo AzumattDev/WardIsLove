f3d3a58922c6630281cb75d7039637397ec2b97e
AzumattDev <azumattgaming@gmail.com>
2022-02-06 20:29:58 -0500

    Verison 2.3.3

--- WardIsLove.cs
+++ WardIsLove.cs
@@ -53,7 +53,7 @@ namespace WardIsLove
             BetterWardType4 = 7,
         }
 
-        public const string version = "2.3.2";
+        public const string version = "2.3.3";
         public const string ModName = "WardIsLove";
         internal const string Author = "Azumatt";
         internal const string HGUID = Author + "." + "WardIsLove";
